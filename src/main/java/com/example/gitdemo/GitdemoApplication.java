3ca16f379d1ed69aae93c8a6553ff10b1d41c79e
Murali <murali@Muralis-MacBook-Air.local>
2018-09-21 01:36:03 +0530

    changes made in main method

--- src/main/java/com/example/gitdemo/GitdemoApplication.java
+++ src/main/java/com/example/gitdemo/GitdemoApplication.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GitdemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("inside");
+		System.out.println("inside demo");
 		SpringApplication.run(GitdemoApplication.class, args);
 	}
 
